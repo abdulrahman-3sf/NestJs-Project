ce22df0c3dc6917950fe62169e4e2af32b50478a
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-03 22:25:44 +0300

    using services findAll and findOne in controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { ITask } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
     constructor(private readonly taskService: TasksService) {}
 
     @Get()
-    public findAll() : string[] {
-        return ['a', 'b', 'c'];
+    public findAll() : ITask[] {
+        return this.taskService.findAll();
     }
 
     @Get('/:id')
-    public findOne(@Param('id') id:number) : string {
-        return `The number is ${id}`;
+    public findOne(@Param('id') id:string) : ITask {
+        return this.taskService.findOne(id);
     }
 }
