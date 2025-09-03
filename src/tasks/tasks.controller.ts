1ad3b3e4b748157ebd21a39e380c7c650bd8a1ba
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-03 22:08:39 +0300

    add new service

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+    constructor(private readonly taskService: TasksService) {}
+
     @Get()
     public findAll() : string[] {
         return ['a', 'b', 'c'];
+    }
+
+    @Get('/:id')
+    public findOne(@Param('id') id:number) : string {
+        return `The number is ${id}`;
     }
 }
