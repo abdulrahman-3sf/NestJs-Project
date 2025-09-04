3d66ab1ea4ad2b94249ce1614aebe0855672b953
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-04 11:35:27 +0300

    builtproof validation, validation with pipes

--- src/tasks/create-task.dto.ts
+++ src/tasks/create-task.dto.ts
@@ -1,7 +1,16 @@
+import { IsEnum, IsNotEmpty, IsString } from "class-validator";
 import { enTaskStatus } from "./task.model";
 
 export class CreateTaskDTO {
+    @IsNotEmpty()
+    @IsString()
     title: string;
+
+    @IsNotEmpty()
+    @IsString()
     description: string;
+
+    @IsNotEmpty()
+    @IsEnum(enTaskStatus)
     status: enTaskStatus;
 }
