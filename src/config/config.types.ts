25570ccb88e0358d5fe3e6ddef078f0263e0d9aa
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-04 23:06:26 +0300

    database configuration with typeORM and environment variables

--- src/config/config.types.ts
+++ src/config/config.types.ts
@@ -7,5 +7,10 @@ export interface ConfigType {
 }
 
 export const appConfigSchema = Joi.object({
-    APP_MESSAGE_PREFIX: Joi.string().default('Hello World!')
+    APP_MESSAGE_PREFIX: Joi.string().default('Hello World!'),
+    DB_HOST: Joi.string().default('localhost'),
+    DB_PORT: Joi.string().default('5432'),
+    DB_USER: Joi.string().required(),
+    DB_PASSWORD: Joi.string().required(),
+    DB_DATABASE: Joi.string().required(),
 });
