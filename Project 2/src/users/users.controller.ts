5d7d202cc172fc6fdbfac10364dbd48a5007ca20
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-06 00:54:27 +0300

    add findAll and findUserByID features + know about dependency injection

--- Project 2/src/users/users.controller.ts
+++ Project 2/src/users/users.controller.ts
@@ -1,18 +1,17 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+    constructor(private usersService: UsersService) {}
+
     @Get()
     getUsers(): any {
-        return {
-            id: 0,
-        };
+        return this.usersService.findAll();
     }
 
     @Get(':id')
     getUserById(@Param('id') id: string): any {
-        return {
-            id: id,
-        }
+        return this.usersService.findUserByID(Number(id)); // TODO: auto parse id
     }
 }
--- Project 2/src/users/users.service.ts
+++ Project 2/src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: any = [{
+        id: 0,
+        name: 'Abdulrahman',
+    }]
+
+    findAll() {
+        return this.users;
+    }
+
+    findUserByID(userID: number) {
+        return this.users.find(user => user.id == userID);
+    }
+}
