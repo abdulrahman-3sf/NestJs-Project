b8ee973f7b31949626c0a15c181b57f5e3d52224
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-06 01:18:17 +0300

    add dto, entities and createUser feature

--- Project 2/src/users/users.service.ts
+++ Project 2/src/users/users.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-    private users: any = [{
+    private users: User[] = [{
         id: 0,
         name: 'Abdulrahman',
-    }]
+    }, {
+        id: 1,
+        name: 'Fasil',
+    }];
 
-    findAll() {
+    findAll(): User[] {
         return this.users;
     }
 
-    findUserByID(userID: number) {
+    findUserByID(userID: number): User | undefined {
         return this.users.find(user => user.id == userID);
+    }
+
+    createUser(createUserDto: CreateUserDto): User {
+        const newUser = {
+            id: this.users.length,
+            ...createUserDto,
+        };
+
+        this.users.push(newUser);
+        return newUser;
     }
 }
