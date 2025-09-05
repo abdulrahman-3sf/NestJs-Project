d7358fb94a56f95f5db8a2077178615f6a986248
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-06 01:37:33 +0300

    swagger openAPI docs

--- Project 2/src/users/users.controller.ts
+++ Project 2/src/users/users.controller.ts
@@ -2,21 +2,26 @@ import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Users')
 @Controller('users')
 export class UsersController {
     constructor(private usersService: UsersService) {}
 
+    @ApiOkResponse({type: User, isArray: true})
     @Get()
     getUsers(): User[] {
         return this.usersService.findAll();
     }
 
+    @ApiOkResponse({type: User})
     @Get(':id')
     getUserById(@Param('id') id: string): User | undefined {
         return this.usersService.findUserByID(Number(id)); // TODO: auto parse id
     }
 
+    @ApiCreatedResponse({type: User})
     @Post()
     createUser(@Body() body: CreateUserDto): User {
         return this.usersService.createUser(body);
