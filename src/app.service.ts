4189fac99fcac3057f7cf938c229203905a88d3e
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-05 18:13:48 +0300

    creating type-safe config service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,16 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { SomethingService } from './something/something.service';
 import { LoggerService } from './logger/logger.service';
-import { ConfigService } from '@nestjs/config';
-import { ConfigType } from './config/config.types';
 import { AppConfig } from './config/app.config';
+import { TypedConfigService } from './config/typed.config.service';
 
 @Injectable()
 export class AppService {
   constructor(
     private readonly somethingService: SomethingService,
     private readonly loggerService: LoggerService,
-    private readonly configService: ConfigService<ConfigType>,
+    private readonly configService: TypedConfigService,
   ) {}
 
   getHello(): string {
