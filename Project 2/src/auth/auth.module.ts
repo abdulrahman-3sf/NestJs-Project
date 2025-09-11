1e7197f65eaac30b7959b566232348d992f46141
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-11 12:25:50 +0300

    implementing passport-local strategy (username and password login)

--- Project 2/src/auth/auth.module.ts
+++ Project 2/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ConsumersModule } from 'src/consumers/consumers.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
 
 @Module({
-  imports: [ConsumersModule],
-  providers: [AuthService]
+  imports: [ConsumersModule, PassportModule],
+  providers: [AuthService, LocalStrategy]
 })
 export class AuthModule {}
